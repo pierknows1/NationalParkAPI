5883880b26dd53d84bf7b0ba2af8ecb02caefcb2
pier <pierknows@gmail.com>
2023-08-27 15:35:13 -0400

    fixed controllers and updated db

--- NationalParks/Controllers/ParksController.cs
+++ NationalParks/Controllers/ParksController.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using NationalParks.Models;
 
-
 namespace NationalParks.Controllers
 {
 
@@ -20,26 +19,104 @@ namespace NationalParks.Controllers
     {
       _db = db;
     }
-    // GET api/parks
-    // [HttpGet]
-    // public async Task<ActionResult<IEnumerable<Park>>> Get (string ParkName, string ParkCity, string ParkState)
-    // {
-    //   IQueryable<Park> query = _db.Parks.AsQueryable();
-      
-    //   if (ParkName != null)
-    //   {
-    //     query = query.Where(entry => entry.ParkName == ParkName);
-    //   }
-    //    if (ParkCity != null)
-    //   {
-    //     query = query.Where(entry => entry.ParkCity == ParkCity);
-    //   }
-    //   if (ParkState != null)
-    //   {
-    //     query = query.Where(entry => entry.ParkState == ParkState);
-    //   }
     
-    //   return await query.ToListAsync();
-    // }
+    //GET api/parks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get (string parkName, string parkCity, string parkState)
+    {
+      IQueryable<Park> query = _db.Parks.AsQueryable();
+      
+      if (parkName != null)
+      {
+        query = query.Where(e => e.ParkName == parkName);
+      }
+       if (parkCity != null)
+      {
+        query = query.Where(e => e.ParkCity == parkCity);
+      }
+      if (parkState != null)
+      {
+        query = query.Where(e => e.ParkState == parkState);
+      }
+    
+      return await query.ToListAsync();
+    }
+
+    // Post api/parks
+    [HttpPost]
+    public async Task<ActionResult<Park>> Post(Park park)
+    {
+      _db.Parks.Add(park);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(Post), new { id = park.ParkId }, park);
+    }
+
+
+    //Get api/Parks/2
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+      
+      if (park == null)
+      {
+          return NotFound();
+      }
+      return park;
+    }
+
+    //put api/Parks/2
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Parks.Update(park);
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
+    
+    }
+
+    // DELETE: api/Parks/2
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }
